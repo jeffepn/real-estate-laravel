365f86e7806ac4fb2b44e4389c12d3e73b20fe5f
jeffepereira <jefferson.pereira.nascimento10@gmail.com>
2021-07-12 09:55:52 -0300

    Add search in property

---
+++ database/migrations/2021_07_12_124701_add_search_in_property.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddSearchInProperty extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (env("DB_CONNECTION") != "sqlite") {
+            DB::statement("ALTER TABLE properties ADD FULLTEXT fulltextproperties_index (min_description, content)");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('properties', function ($table) {
+            $table->dropIndex('fulltextproperties_index');
+        });
+    }
+}
