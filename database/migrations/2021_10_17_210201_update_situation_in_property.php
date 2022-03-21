4f20c2dcbda0c81c6e660a7bd5e8d98aa9010e28
jeffepereira <jefferson.pereira.nascimento10@gmail.com>
2022-03-20 23:57:49 -0300

    fix bug in migration

--- database/migrations/2021_10_17_210201_update_situation_in_property.php
+++ database/migrations/2021_10_17_210201_update_situation_in_property.php
@@ -15,12 +15,10 @@ class UpdateSituationInProperty extends Migration
      */
     public function up()
     {
-        $situation = Situation::whereSlug('pronto')->first();
-        if ($situation && Property::whereSituationId($situation->id)->count() < 1) {
-            $situation->delete();
-        }
         Schema::table('properties', function (Blueprint $table) {
             $table->dropForeign(['situation_id']);
+        });
+        Schema::table('properties', function (Blueprint $table) {
             $table->string('situation_id')->default(null)->change();
             $table->foreign('situation_id')
                 ->references('id')
@@ -38,6 +36,8 @@ class UpdateSituationInProperty extends Migration
         $situation = Situation::firstOrCreate(['name' => 'Pronto']);
         Schema::table('properties', function (Blueprint $table) use ($situation) {
             $table->dropForeign(['situation_id']);
+        });
+        Schema::table('properties', function (Blueprint $table) use ($situation) {
             $table->string('situation_id')->default($situation->id)->change();
             $table->foreign('situation_id')
                 ->references('id')
