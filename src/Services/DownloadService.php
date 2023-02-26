a0c7b4bdeaa71a71691c4f716e6599c331a104cc
jeffepereira <jefferson.pereira.nascimento10@gmail.com>
2023-02-26 03:01:23 -0300

    [REL-5 improv(images): remove old files zip

--- src/Services/DownloadService.php
+++ src/Services/DownloadService.php
@@ -2,19 +2,21 @@
 
 namespace Jeffpereira\RealEstate\Services;
 
+use Illuminate\Support\Facades\Storage;
 use ZipArchive;
 
 class DownloadService
 {
-    private const PATH_ZIP = 'app/zip-downloads';
+    private const PATH_ZIP = 'zip-downloads';
 
-    public function createZipDownload(array $wayFiles): string
+    public function createZipDownload(array $wayFiles, $deleteFile = true): string
     {
         $zip = new ZipArchive();
-        $fileNameZip = self::PATH_ZIP . '/files_' . time() . '.zip';
+        $fileNameZip = sprintf("app/%s/files_%d.zip", self::PATH_ZIP, time());
+        $this->cleanDirectory();
         $this->checkAndCreateDirectoryZip();
 
-        if (true === ($zip->open(storage_path($fileNameZip), ZipArchive::CREATE|ZipArchive::OVERWRITE))) {
+        if (true === ($zip->open(storage_path($fileNameZip), ZipArchive::CREATE | ZipArchive::OVERWRITE))) {
             foreach ($wayFiles as $way) {
                 $relativeName = basename($way);
                 $zip->addFile($way, $relativeName);
@@ -27,8 +29,11 @@ class DownloadService
 
     private function checkAndCreateDirectoryZip(): void
     {
-        if (!file_exists(storage_path(self::PATH_ZIP))) {
-            mkdir(storage_path(self::PATH_ZIP), 0755, true);
-        }
+        Storage::makeDirectory(self::PATH_ZIP);
+    }
+
+    private function cleanDirectory(): void
+    {
+        Storage::delete(Storage::allFiles(self::PATH_ZIP));
     }
 }
